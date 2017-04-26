64b99793ab788131c42e8e4b89c32354802a54aa
Max Starikevich <maxim.starikevich@gmail.com>
2017-04-27 00:00:41 +0300

    Config setup task will start only after docker run

--- run.sh
+++ run.sh
@@ -19,24 +19,49 @@ shutdownSystem()
 	service crond stop
 }
 
-if [ ! -z "$TIMEZONE" ];
+startConfigurationTask()
+{
+    if [ ! -z "$TIMEZONE" ];
+    then
+        cp -f /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    fi
+
+    if [[ $MULTISITE_ID -gt 0 ]];
+    then
+        find /etc/ -type f -exec sed -i "s/\/home\/bitrix\/www/\/home\/bitrix\/www${MULTISITE_ID}/g" {} \;
+    fi
+
+    if [ $CYRILLIC_MODE -eq 1 ];
+    then
+        sed -i '/mbstring.func_overload/c\mbstring.func_overload = 0' /etc/php.d/bitrixenv.ini
+        sed -i '/mbstring.internal_encoding/c\mbstring.internal_encoding = cp1251' /etc/php.d/bitrixenv.ini
+    fi
+
+    if [[ $NOMYSQL -eq 1 ]];
+    then
+        echo "Internal MySQL is offline. Use your own DB-server instead... (NOMYSQL=1)"
+    else
+        service mysqld start
+
+        # setting new root password for mysql + allowing to connect internal mysql-server from outside
+        mysql -u root -e "use mysql; UPDATE user SET password=PASSWORD('$BITRIX_DB_PASS') WHERE User='bitrix'; flush privileges; GRANT ALL ON *.* to bitrix@'%' IDENTIFIED BY '$BITRIX_DB_PASS'; GRANT ALL ON *.* to bitrix@'localhost' IDENTIFIED BY '$BITRIX_DB_PASS';"
+
+        if [ ! -z "$DB_NAME" ];
+        then
+            mysql -u bitrix -p$BITRIX_DB_PASS -e "create database $DB_NAME $DB_ADDITIONAL_PARAMS";
+        fi
+
+        service mysqld stop
+    fi
+
+    sed -i '/AVAILABLE_MEMORY=$(free/c\AVAILABLE_MEMORY=262144' /etc/init.d/bvat
+}
+
+if [ ! -f "/home/bitrix/dockerInstanceStarted" ];
 then
-    cp -f /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    startConfigurationTask
+    touch /home/bitrix/dockerInstanceStarted
 fi
-
-if [[ $MULTISITE_ID -gt 0 ]];
-then
-    find /etc/ -type f -exec sed -i "s/\/home\/bitrix\/www/\/home\/bitrix\/www${MULTISITE_ID}/g" {} \;
-fi
-
-if [ $CYRILLIC_MODE -eq 1 ];
-then
-    sed -i '/mbstring.func_overload/c\mbstring.func_overload = 0' /etc/php.d/bitrixenv.ini
-    sed -i '/mbstring.internal_encoding/c\mbstring.internal_encoding = cp1251' /etc/php.d/bitrixenv.ini
-fi
-
-sed -i '/AVAILABLE_MEMORY=$(free/c\AVAILABLE_MEMORY=262144' /etc/init.d/bvat
-
 
 # start service in background here
 /etc/init.d/bvat start
@@ -46,14 +71,6 @@ then
     echo "Internal MySQL is offline. Use your own DB-server instead... (NOMYSQL=1)"
 else
     service mysqld start
-
-    # setting new root password for mysql + allowing to connect internal mysql-server from outside
-	mysql -u root -e "use mysql; UPDATE user SET password=PASSWORD('$BITRIX_DB_PASS') WHERE User='bitrix'; flush privileges; GRANT ALL ON *.* to bitrix@'%' IDENTIFIED BY '$BITRIX_DB_PASS'; GRANT ALL ON *.* to bitrix@'localhost' IDENTIFIED BY '$BITRIX_DB_PASS';"
-
-	if [ ! -z "$DB_NAME" ];
-	then
-	    mysql -u bitrix -p$BITRIX_DB_PASS -e "create database $DB_NAME $DB_ADDITIONAL_PARAMS";
-	fi
 fi
 
 service crond start
